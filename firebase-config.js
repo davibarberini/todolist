67ec53255bf8ed00effa80699ce9b8293d88893c
Davi Barberini <davibarberini@192.168.1.16>
2026-09-01 18:57:32 -0300

    firebase auth and storage

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Preencha com os dados do Firebase Console (Project settings > Your apps).
+const firebaseConfig = {
+  apiKey: "AIzaSyC3zTmGx_1dhe2QnJim7_-gYeFBUeefRrQ",
+  authDomain: "todo-dn-list.firebaseapp.com",
+  projectId: "todo-dn-list",
+  storageBucket: "todo-dn-list.firebasestorage.app",
+  messagingSenderId: "960117001733",
+  appId: "1:960117001733:web:be5309d9e56542f384566c",
+  measurementId: "G-DW0X5795GY"
+};
--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = "todolist-v1";
+const CACHE_NAME = "todolist-v2";
 const ASSETS = [
   "./index.html",
   "./manifest.webmanifest",
+  "./firebase-config.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./icons/icon.svg",
